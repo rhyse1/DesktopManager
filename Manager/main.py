89b60c0a2cfd4d62b62290844b6d0b610a92de4e
Rhys Evans <mrevinz@gmail.com>
2020-01-09 13:37:55 +0000

    minor tweaks

--- Manager/main.py
+++ Manager/main.py
@@ -2,7 +2,6 @@ from tkinter import *
 from tkinter import messagebox
 
 import CleanDesktop
-import SettingsMenu
 
 Height = 1
 Width = 20
@@ -11,18 +10,18 @@ def main():
     window = Tk()
 
     window.title("Desktop Cleaner - Main Menu")
-    window.geometry("310x120")
-    window.resizable(0,0) 
+    window.geometry("250x90")
+    window.resizable(0,0)
     window.eval('tk::PlaceWindow %s center' % window.winfo_pathname(window.winfo_id()))
 
     menuLbl = Label(window, text = "Desktop Cleaner", font = ('Century Gothic',22))
-    menuLbl.grid(columnspan = 4) 
+    menuLbl.grid(columnspan = 4)
 
     cleanBttn = Button(text = "Clean Desktop", fg= "Black", command = lambda:cleanBttn_OnClicked(), font = ('Century Gothic',18), height = Height, width = Width)
-    cleanBttn.grid(columnspan = 4) 
+    cleanBttn.grid(columnspan = 4)
 
     copyLbl = Label(window, text = "Rhys Evans, 2019", font = ('Century Gothic',10))
-    copyLbl.grid(columnspan = 4) 
+    copyLbl.grid(columnspan = 4)
 
     window.mainloop()
 
@@ -30,7 +29,7 @@ def main():
 def cleanBttn_OnClicked():
     CleanDesktop.Clean()
 
-	
+
 
 if __name__ == '__main__':
 	main()
